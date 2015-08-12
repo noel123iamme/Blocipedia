a0fd368daec2f5c92261b8a0c1fd126778ed4b41
Noel Deguzman <noel.deguzman@me.com>
2015-08-11 21:57:38 -0700

    upgrading an account

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,45 +1,45 @@
 class WikisController < ApplicationController
   def index
-  	@wikis = Wiki.all
-  	authorize @wikis
+    @wikis = Wiki.all
+    authorize @wikis
   end
 
   def show
-  	@wiki = Wiki.find(params[:id])
-  	authorize @wiki
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
   end
 
   def new
-  	@wiki = Wiki.new
-  	authorize @wiki
+    @wiki = Wiki.new
+    authorize @wiki
   end
 
-   def create
-     @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
-     authorize @wiki
-     if @wiki.save
-       flash[:notice] = "Wiki was saved."
-       redirect_to @wiki
-     else
-       flash[:error] = "There was an error saving the wiki. Please try again."
-       render :new
-     end
-   end
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    authorize @wiki
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
 
   def edit
-     @wiki = Wiki.find(params[:id])
-     authorize @wiki
-   end
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+  end
 
-   def update
-     @wiki = Wiki.find(params[:id])
-     authorize @wiki
-     if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
-       flash[:notice] = "Wiki was updated."
-       redirect_to @wiki
-     else
-       flash[:error] = "There was an error saving the wiki. Please try again."
-       render :edit
-     end
-   end
+  def update
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :wikis
+  resources :charges, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
